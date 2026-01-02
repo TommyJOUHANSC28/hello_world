0141fb971798aa5dab70331cec0e9654bced1665
Tommy JOUHANS <12119@holbertonstudents.com>
2026-01-02 12:07:59 +0000

    amelioration de del_space

--- del_space.c
+++ del_space.c
@@ -9,14 +9,19 @@
 char *del_space(char *str)
 {
 char *end;
+if (!str)
+return (NULL);
+/* Sauter les espaces/tabs au début */
 while (*str == ' ' || *str == '\t')
 str++;
 if (*str == '\0')
 return (str);
+/* Trouver la fin */
 end = str;
 while (*end)
 end++;
 end--;
+/* Supprimer les espaces/tabs à la fin */
 while (end > str && (*end == ' ' || *end == '\t'))
 {
 *end = '\0';
