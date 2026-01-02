b07231f46ba451e1c51b9da823779d07aedbd58d
Tommy JOUHANS <12119@holbertonstudents.com>
2026-01-02 11:25:37 +0000

    amelioration de getenv

--- _getenv.c
+++ _getenv.c
@@ -11,17 +11,17 @@
  * or NULL if there is no match
  *
  */
-char *_getenv(const char *name, char **envp)
+char *_getenv(const char *name)
 {
 int i;
 size_t len;
-if (!name || !envp)
+if (!name)
 return (NULL);
 len =_strlen(name);
-for (i = 0; envp[i] != 0; i++)
+for (i = 0; environ[i] != 0; i++)
 {
-if (_strncmp(envp[i], name, len) == 0 && envp[i][len] == '=')
-return (envp[i] + len + 1);
+if (_strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
+return (environ[i] + len + 1);
 }
 return (NULL);
 }
