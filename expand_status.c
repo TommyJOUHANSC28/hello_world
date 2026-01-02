c09b5836d87c95c628603e786dcdef68657ad375
Tommy JOUHANS <12119@holbertonstudents.com>
2026-01-02 12:26:58 +0000

    amelioration de expand_status

--- expand_status.c
+++ expand_status.c
@@ -1,13 +1,15 @@
 #include "shell.h"
 /**
-* expand_status - fonction $? retour de la dernière commande 
-* @line: poiteur à la line  
-* Return: la dernière commande exécutée
+* expand_status - remplace $? par le code de retour précédent
+* @line: chaîne d'entrée
+ * Return: nouvelle chaîne avec $? substitué, ou line si pas de $?.
 */
 char *expand_status(char *line)
 {
 char buf[16];
 char *pos;
+if(!line)
+return (NULL);
 pos = _strstr(line, "$?");
 if (!pos)
 return (line);
